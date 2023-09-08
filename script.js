ea3d001628ef6bceea1b2fb6c18ce8e0b19c379b
5Akan <akanimohsobot@gmail.com>
2023-09-08 13:20:21 +0100

    Made changes

--- script.js
+++ script.js
@@ -8,23 +8,23 @@ const h3 = document.querySelector('h3');
 h3.innerText = currentDayOfTheWeek;
 
 
-const hourEl = document.getElementById('hour');
-const minEl = document.getElementById('minutes');
-const secEl = document.getElementById('seconds');
+// const hourEl = document.getElementById('hour');
+// const minEl = document.getElementById('minutes');
+// const secEl = document.getElementById('seconds');
 const milliEl = document.getElementById('milliseconds');
 
 
 // function format(time) {
 //   return time < 10 ? (`0${time}`) : time;
 // }
-const hour = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-const seconds = currentDate.getSeconds();
+// const hour = currentDate.getHours();
+// const minutes = currentDate.getMinutes();
+// const seconds = currentDate.getSeconds();
 const milliseconds = currentDate.getMilliseconds();
 
-hourEl.innerText = hour;
-minEl.innerText = minutes;
-secEl.innerText = seconds;
+// hourEl.innerText = hour;
+// minEl.innerText = minutes;
+// secEl.innerText = seconds;
 milliEl.innerText = milliseconds;
 }
-setInterval(displayTime, 100)
+setInterval(displayTime)
